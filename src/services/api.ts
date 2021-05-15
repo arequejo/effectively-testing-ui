f7c90c1e30a77450fad7fe288fcd3714aedd6794
Alejandro Requejo <arequef@gmail.com>
2021-05-15 13:21:22 -0600

    feat: update api service to ts and add saveContact method

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
+import { FormContact } from '../support/types';
 
 axios.defaults.baseURL = 'http://localhost:3001';
 
 export function getContacts() {
   return axios.get('/contacts').then(({ data }) => data);
+}
+
+export function saveContact(contact: FormContact) {
+  return axios.post('/contacts', contact).then(({ data }) => data);
 }
