842e63f825432039747740dd9619cc281cef11c7
Alejandro Requejo <arequef@gmail.com>
2021-05-14 21:38:20 -0600

    fix: remove fields from contact form

--- src/components/Contacts/ContactForm.tsx
+++ src/components/Contacts/ContactForm.tsx
@@ -1,5 +1,5 @@
 import Modal from './Modal';
-import { Control, Label, Input, Button, ButtonGroup } from './styled';
+// import { Control, Label, Input, Button, ButtonGroup } from './styled';
 
 type ContactFormProps = {
   isOpen: boolean;
@@ -9,32 +9,7 @@ type ContactFormProps = {
 function ContactForm({ isOpen, onClose }: ContactFormProps) {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <Control>
-        <Label>Photo</Label>
-        <Input />
-      </Control>
-
-      <Control>
-        <Label>Name</Label>
-        <Input />
-      </Control>
-
-      <Control>
-        <Label>Email</Label>
-        <Input />
-      </Control>
-
-      <Control>
-        <Label>Phone number</Label>
-        <Input />
-      </Control>
-
-      <Control>
-        <ButtonGroup>
-          <Button variant="secondary">Cancel</Button>
-          <Button variant="primary">Save</Button>
-        </ButtonGroup>
-      </Control>
+      TODO
     </Modal>
   );
 }
