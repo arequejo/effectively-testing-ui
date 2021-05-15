61bd93ac81bd2f1d8e22ab3b8720ffd12581cfae
Alejandro Requejo <arequef@gmail.com>
2021-05-15 14:09:25 -0600

    feat: setup contact form component and tests

--- src/components/Contacts/ContactForm.tsx
+++ src/components/Contacts/ContactForm.tsx
@@ -1,12 +1,26 @@
+import * as React from 'react';
 import Modal from './Modal';
+// import Alert from './Alert';
 // import { Control, Label, Input, Button, ButtonGroup } from './styled';
+import { FormContact } from '../../support/types';
+// import { Status } from '../../support/enums';
+// import { saveContact } from '../../services/api';
 
 type ContactFormProps = {
   isOpen: boolean;
   onClose: () => void;
 };
 
+const initialContactData: FormContact = {
+  photo: '',
+  name: '',
+  email: '',
+  phoneNumber: '',
+};
+
 function ContactForm({ isOpen, onClose }: ContactFormProps) {
+  const [contact, setContact] = React.useState<FormContact>(initialContactData);
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       TODO
---
+++ src/components/Contacts/__tests__/ContactForm.tsx
@@ -0,0 +1,71 @@
+// Testing library
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+// MSW
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+import ContactForm from '../ContactForm';
+import { FormContact } from '../../../support/types';
+
+// Component props
+const props = {
+  isOpen: true,
+  onClose: jest.fn(),
+};
+
+// Dummy contact data
+const contact: FormContact = {
+  photo: 'https://loremflickr.com/100/100?random=3',
+  name: 'Foo Bar',
+  email: 'foo.bar@example.com',
+  phoneNumber: '6561221123',
+};
+
+// MSW setup
+const baseUrl = 'http://localhost:3001';
+const contactsEndpoint = `${baseUrl}/contacts`;
+
+const server = setupServer(
+  rest.post(contactsEndpoint, (req, res, ctx) => {
+    return res(ctx.status(201));
+  })
+);
+
+// Jest global methods
+beforeAll(() => {
+  server.listen();
+});
+afterEach(() => {
+  server.resetHandlers();
+  props.onClose.mockReset();
+});
+afterAll(() => {
+  server.close();
+});
+
+describe('ContactForm component', () => {
+  it('submits the form successfully', async () => {
+    // Render the component
+    render(<ContactForm {...props} />);
+
+    // Fill out fields
+
+    // Submit the form
+
+    // Validate fields have been reset
+  });
+
+  it('handles server errors when submitting the form', async () => {
+    // Update the default handler so it returns a 500
+
+    // Render the component
+    render(<ContactForm {...props} />);
+
+    // Submit the form
+
+    // Validate alert
+  });
+});
