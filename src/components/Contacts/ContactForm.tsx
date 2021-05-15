e7a19ea822b3216fa3ceda615e412b102c848726
Alejandro Requejo <arequef@gmail.com>
2021-05-15 14:39:13 -0600

    feat: implement contact form component and tests

--- src/components/Contacts/ContactForm.tsx
+++ src/components/Contacts/ContactForm.tsx
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as React from 'react';
 import Modal from './Modal';
-// import Alert from './Alert';
-// import { Control, Label, Input, Button, ButtonGroup } from './styled';
+import Alert from './Alert';
+import { Control, Label, Input, Button, ButtonGroup } from './styled';
 import { FormContact } from '../../support/types';
-// import { Status } from '../../support/enums';
-// import { saveContact } from '../../services/api';
+import { Status } from '../../support/enums';
+import { saveContact } from '../../services/api';
 
 type ContactFormProps = {
   isOpen: boolean;
@@ -22,10 +21,92 @@ const initialContactData: FormContact = {
 
 function ContactForm({ isOpen, onClose, onSave }: ContactFormProps) {
   const [contact, setContact] = React.useState<FormContact>(initialContactData);
+  const [status, setStatus] = React.useState<Status>(Status.Idle);
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>): void => {
+    const target = event.target as HTMLInputElement;
+    setContact({
+      ...contact,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    setStatus(Status.Pending);
+    saveContact(contact)
+      .then(() => {
+        setContact(initialContactData);
+        setStatus(Status.Resolved);
+        onSave();
+      })
+      .catch(() => {
+        setStatus(Status.Rejected);
+      });
+  };
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      TODO
+      {status === Status.Rejected && (
+        <Alert type="error" message="Uh oh! Looks like something went wrong" />
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <Control>
+          <Label htmlFor="photo">Photo URL</Label>
+          <Input
+            id="photo"
+            type="url"
+            name="photo"
+            value={contact.photo}
+            onChange={handleChange}
+          />
+        </Control>
+
+        <Control>
+          <Label htmlFor="name">Name</Label>
+          <Input
+            id="name"
+            type="text"
+            name="name"
+            value={contact.name}
+            onChange={handleChange}
+          />
+        </Control>
+
+        <Control>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            name="email"
+            value={contact.email}
+            onChange={handleChange}
+          />
+        </Control>
+
+        <Control>
+          <Label htmlFor="phoneNumber">Phone number</Label>
+          <Input
+            id="phoneNumber"
+            type="tel"
+            name="phoneNumber"
+            value={contact.phoneNumber}
+            onChange={handleChange}
+          />
+        </Control>
+
+        <Control>
+          <ButtonGroup>
+            <Button variant="secondary" type="button" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button variant="primary" disabled={status === Status.Pending}>
+              Save
+            </Button>
+          </ButtonGroup>
+        </Control>
+      </form>
     </Modal>
   );
 }
--- src/components/Contacts/__tests__/ContactForm.tsx
+++ src/components/Contacts/__tests__/ContactForm.tsx
@@ -43,6 +43,7 @@ beforeAll(() => {
 afterEach(() => {
   server.resetHandlers();
   props.onClose.mockReset();
+  props.onSave.mockReset();
 });
 afterAll(() => {
   server.close();
@@ -54,20 +55,60 @@ describe('ContactForm component', () => {
     render(<ContactForm {...props} />);
 
     // Fill out fields
+    const photoField = screen.getByLabelText(/photo/i);
+    expect(photoField).toBeInTheDocument();
+    userEvent.type(photoField, contact.photo);
+    expect(photoField).toHaveValue(contact.photo);
+
+    const nameField = screen.getByLabelText(/name/i);
+    expect(nameField).toBeInTheDocument();
+    userEvent.type(nameField, contact.name);
+    expect(nameField).toHaveValue(contact.name);
+
+    const emailField = screen.getByLabelText(/email/i);
+    expect(emailField).toBeInTheDocument();
+    userEvent.type(emailField, contact.email);
+    expect(emailField).toHaveValue(contact.email);
+
+    const phoneNumberField = screen.getByLabelText(/phone number/i);
+    expect(phoneNumberField).toBeInTheDocument();
+    userEvent.type(phoneNumberField, contact.phoneNumber);
+    expect(phoneNumberField).toHaveValue(contact.phoneNumber);
 
     // Submit the form
+    const submitButton = screen.getByRole('button', { name: /save/i });
+    userEvent.click(submitButton);
+    expect(submitButton).toBeDisabled();
 
     // Validate fields have been reset
+    await waitFor(() => expect(photoField).not.toHaveValue());
+    expect(nameField).not.toHaveValue();
+    expect(emailField).not.toHaveValue();
+    expect(phoneNumberField).not.toHaveValue();
+    expect(submitButton).toBeEnabled();
+    expect(props.onSave).toHaveBeenCalled();
   });
 
   it('handles server errors when submitting the form', async () => {
     // Update the default handler so it returns a 500
+    server.use(
+      rest.post(contactsEndpoint, (req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
 
     // Render the component
     render(<ContactForm {...props} />);
 
     // Submit the form
+    const submitButton = screen.getByRole('button', { name: /save/i });
+    userEvent.click(submitButton);
+    expect(submitButton).toBeDisabled();
 
     // Validate alert
+    const alert = await screen.findByRole('alert');
+    expect(alert).toBeInTheDocument();
+    expect(alert).toHaveTextContent(/something went wrong/i);
+    expect(submitButton).toBeEnabled();
   });
 });
